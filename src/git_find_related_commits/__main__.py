9afc562b37904ab692604862d2250f5597668385
Antti Kaihola <13725+akaihola@users.noreply.github.com>
2023-01-09 22:00:17 +0200

    Functions instead of a helper class
    Also handle commit `str`/`bytes` correctly. Mypy likes this.

--- src/git_find_related_commits/__main__.py
+++ src/git_find_related_commits/__main__.py
@@ -20,117 +20,120 @@ import re
 from typing import Generator, Iterable
 
 import git  # pip install GitPython
+from git.objects import Commit
 
 TEMPORARY_BRANCH_NAME = "tmp-find-related-commits"
 
 
-class GitHelper:
-    def __init__(self, repo_dir: str):
-        self.repo = git.Repo(repo_dir)
+def get_main_branch(repo: git.Repo) -> str:
+    """Get the remote main branch name"""
+    symref = repo.git.ls_remote("--symref", "origin", "HEAD")
+    match = re.match(r"ref: refs/heads/(.+)\tHEAD\b", symref)
+    if not match:
+        raise RuntimeError("Can't parse `git --symref origin HEAD` output {symref!r}")
+    head = match.group(1)
+    return f"origin/{head}"
 
-        # get the main branch name
-        symref_output = self.repo.git.ls_remote("--symref", "origin", "HEAD")
-        match = re.match(r"ref: refs/heads/(.+)\tHEAD\b", symref_output)
-        if not match:
-            raise RuntimeError(
-                "Can't parse `git --symref origin HEAD` output {symref_output!r}"
-            )
-        head = match.group(1)
-        self.main_branch = f"origin/{head}"
 
-        self.local_branch = self.repo.active_branch
-        assert self.local_branch.name != TEMPORARY_BRANCH_NAME
+def apply_and_diff_commit_pairs(
+    repo: git.Repo, main_branch: str, local_branch: git.Head
+) -> list[tuple[int, int, Commit, Commit]]:
 
-    def get_commit_list(self) -> list[git.Commit]:
-        """
-        Returns:
-        All the commits starting from the main branch.
-        """
-        # On a branch, get the base commit which is in the main branch
-        # (e.g. origin/master).
-        base_commit = self.repo.merge_base(self.main_branch, self.local_branch)[0]
-        return list(
-            reversed(
-                list(self.repo.iter_commits(f"{base_commit}..{self.local_branch}"))
-            )
-        )
+    assert local_branch.name != TEMPORARY_BRANCH_NAME
 
-    def count_changed_lines_since(self, commit0: git.Commit) -> int:
-        diff_str = self.repo.git.diff("--shortstat", f"{commit0}..HEAD")
-        return _get_shortstat_total(diff_str)
-
-    def apply_and_diff_commit_pairs(
-        self,
-    ) -> list[tuple[int, int, git.Commit, git.Commit]]:
-        commits = self.get_commit_list()
-        print_all_commits(commits)
-        print("Iterate...")
-        results = []
-        for i, commit1 in enumerate(commits):
-            (commit0,) = commit1.parents  # will fail if more than one parent
-            # print(f"Start at {_format_commit(commit0)}")
-            with self.in_tmp_branch(commit1):
-                # print(f"Apply {_format_commit(commit1)}")
-                diff_count1 = self.count_changed_lines_since(commit0)
-                if diff_count1 is None:
-                    continue
-                for commit2, rel_diff, rel_diff_c in self.apply_and_diff_each_commit2(
-                    commit0, commit1, diff_count1, commits[i + 1 :]
-                ):
-                    print(f"{rel_diff or '':>4} {_format_commits(commit1, commit2)}")
-                    if rel_diff is not None and rel_diff_c is not None:
-                        results.append((rel_diff_c, rel_diff, commit1, commit2))
-        return results
-
-    def apply_and_diff_each_commit2(
-        self,
-        commit0: git.Commit,
-        commit1: git.Commit,
-        diff_count1: int,
-        commits: Iterable[git.Commit],
-    ) -> Generator[tuple[git.Commit, int | None, int | None], None, None]:
-        for commit2 in commits:
-            self.repo.git.reset("--hard", commit1)
-            # print(f"Apply {_format_commit(commit2)}")
-            try:
-                self.repo.git.cherry_pick(commit2, "--keep-redundant-commits")
-            except git.GitCommandError:
-                yield commit2, None, None
+    commits = get_commit_list(repo, main_branch, local_branch)
+    print_all_commits(commits)
+    print("Iterate...")
+    results = []
+    for i, commit1 in enumerate(commits):
+        (commit0,) = commit1.parents  # will fail if more than one parent
+        # print(f"Start at {_format_commit(commit0)}")
+        with in_tmp_branch(repo, commit1):
+            # print(f"Apply {_format_commit(commit1)}")
+            diff_count1 = count_changed_lines_since(repo, commit0)
+            if diff_count1 is None:
                 continue
-            diff_count2 = self.count_changed_lines_since(commit0)
-            rel_diff = diff_count2 - diff_count1
-            commit_diff_str = self.repo.git.show(commit2, format="", shortstat=True)
-            rel_diff_c = rel_diff - _get_shortstat_total(commit_diff_str)
-            if rel_diff_c >= 0:
-                # this commit has no influence on the prev commit.
-                # so skip this whole proposed squash
-                yield commit2, None, None
-            else:
-                yield commit2, rel_diff, rel_diff_c
+            for commit2, rel_diff, rel_diff_c in apply_and_diff_each_commit2(
+                repo, commit0, commit1, diff_count1, commits[i + 1 :]
+            ):
+                print(f"{rel_diff or '':>4} {_format_commits(commit1, commit2)}")
+                if rel_diff is not None and rel_diff_c is not None:
+                    results.append((rel_diff_c, rel_diff, commit1, commit2))
+    return results
 
-    @contextlib.contextmanager
-    def in_tmp_branch(self, commit: git.Commit) -> Generator[git.Head, None, None]:
-        repo = self.repo
-        prev_active_branch = repo.active_branch
-        tmp_branch = repo.create_head(TEMPORARY_BRANCH_NAME, commit.hexsha, force=True)
-        repo.git.checkout(tmp_branch)
+
+def get_commit_list(
+    repo: git.Repo, main_branch: str, local_branch: git.Head
+) -> list[Commit]:
+    """
+    Returns:
+    All the commits starting from the main branch.
+    """
+    # On a branch, get the base commit which is in the main branch
+    # (e.g. origin/master).
+    base_commit = repo.merge_base(main_branch, local_branch)[0]
+    return list(reversed(list(repo.iter_commits(f"{base_commit}..{local_branch}"))))
+
+
+@contextlib.contextmanager
+def in_tmp_branch(repo: git.Repo, commit: Commit) -> Generator[git.Head, None, None]:
+    prev_active_branch = repo.active_branch
+    tmp_branch = repo.create_head(TEMPORARY_BRANCH_NAME, commit.hexsha, force=True)
+    repo.git.checkout(tmp_branch)
+    try:
+        yield tmp_branch
+    except git.GitCommandError as exc:
+        print("Git exception occurred. Current git status:")
+        print(repo.git.status())
+        raise exc
+    finally:
+        repo.git.reset("--hard")
+        repo.git.checkout(prev_active_branch)
+        repo.delete_head(TEMPORARY_BRANCH_NAME, force=True)
+
+
+def count_changed_lines_since(repo: git.Repo, commit0: Commit) -> int:
+    diff_str = repo.git.diff("--shortstat", f"{commit0}..HEAD")
+    return _get_shortstat_total(diff_str)
+
+
+def apply_and_diff_each_commit2(
+    repo: git.Repo,
+    commit0: Commit,
+    commit1: Commit,
+    diff_count1: int,
+    commits: Iterable[Commit],
+) -> Generator[tuple[Commit, int | None, int | None], None, None]:
+    for commit2 in commits:
+        repo.git.reset("--hard", commit1)
+        # print(f"Apply {_format_commit(commit2)}")
         try:
-            yield tmp_branch
-        except git.GitCommandError as exc:
-            print("Git exception occurred. Current git status:")
-            print(repo.git.status())
-            raise exc
-        finally:
-            repo.git.reset("--hard")
-            repo.git.checkout(prev_active_branch)
-            repo.delete_head(TEMPORARY_BRANCH_NAME, force=True)
+            repo.git.cherry_pick(commit2, "--keep-redundant-commits")
+        except git.GitCommandError:
+            yield commit2, None, None
+            continue
+        diff_count2 = count_changed_lines_since(repo, commit0)
+        rel_diff = diff_count2 - diff_count1
+        commit_diff_str = repo.git.show(commit2, format="", shortstat=True)
+        rel_diff_c = rel_diff - _get_shortstat_total(commit_diff_str)
+        if rel_diff_c >= 0:
+            # this commit has no influence on the prev commit.
+            # so skip this whole proposed squash
+            yield commit2, None, None
+        else:
+            yield commit2, rel_diff, rel_diff_c
 
 
-def _format_commit(commit: git.Commit) -> str:
-    return f"{str(commit):8.8} {commit.message.splitlines()[0]}"
+def _format_commit(commit: Commit) -> str:
+    message = (
+        commit.message.decode("ascii")
+        if isinstance(commit.message, bytes)
+        else commit.message
+    )
+    return f"{str(commit):8.8} {message.splitlines()[0]}"
 
 
-def _format_commits(commit1: git.Commit, commit2: git.Commit) -> str:
+def _format_commits(commit1: Commit, commit2: Commit) -> str:
     return f"{_format_commit(commit1)} -- {_format_commit(commit2)}"
 
 
@@ -163,13 +166,13 @@ def _get_shortstat_total(shortstat_output: str) -> int:
     return int(match.group(1) or 0) + int(match.group(2) or 0)
 
 
-def print_all_commits(commits: Iterable[git.Commit]) -> None:
+def print_all_commits(commits: Iterable[Commit]) -> None:
     print("All commits:")
     for commit in commits:
         print("  ", _format_commit(commit), sep="")
 
 
-def print_results(results: list[tuple[int, int, git.Commit, git.Commit]]) -> None:
+def print_results(results: list[tuple[int, int, Commit, Commit]]) -> None:
     print("Done. Results:")
     results.sort(key=lambda x: x[0])
     for (c_, c, commit1, commit2) in results:
@@ -183,8 +186,9 @@ def print_results(results: list[tuple[int, int, git.Commit, git.Commit]]) -> Non
 
 
 def main() -> None:
-    helper = GitHelper(".")
-    results = helper.apply_and_diff_commit_pairs()
+    repo = git.Repo(".")
+    main_branch = get_main_branch(repo)
+    results = apply_and_diff_commit_pairs(repo, main_branch, repo.active_branch)
     print_results(results)
 
 
